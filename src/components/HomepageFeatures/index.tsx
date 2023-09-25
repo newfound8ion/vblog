cd1b2f80a8191b0d7393dfbf176eed8748d481e2
Sofiane Delloue <112469623+s8fe@users.noreply.github.com>
2023-09-25 19:56:24 +0200

    Update index.tsx

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,32 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'What is Newcoin?',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Newcoin is a log of cryptographically signed credentials that are processed in parallel using a probabilistic 
+        state machine to achieve a shared consensus state powered by the Proof-of-Creativity algorithm. 
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Why do I need it?',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Make your application natively Sybil-resistant and qualitative by leveraging the ocean of data points available 
+        across the World Wide Web into a universal algorithmic endpoint that can be read by smart contracts, backend and clients.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'How does it work?',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Contribute to the algorithm by deploying ActivationFunction contracts that contribute to the Proof-of-Creativity algorithm
+        and reward your users with trust points and query the WATT score of any blockchain wallet to instantly detect awesomeness.
       </>
     ),
   },
