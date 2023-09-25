8ff2925bb16f91e7ebf1d66e87072b3c8cc1c044
Sofiane Delloue <112469623+s8fe@users.noreply.github.com>
2023-09-25 22:32:39 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Newcoin Intro - 5min ⏱️
+            Newcoin Intro
           </Link>
         </div>
       </div>
