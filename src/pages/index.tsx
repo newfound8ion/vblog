097da2f4d2c86b4223f0abffd3f22a669409c81f
Vectoshi Eigenvector Newkamoto <112469623+vectoshi@users.noreply.github.com>
2023-10-01 20:50:44 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,66 +1,37 @@
+// src/pages/index.tsx
+
 import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/getting-started/overview">
-            Get Started With Newcoin
-          </Link>
-        </div>
-        <img src="/img/welcome.png" alt="Welcome to the cutting-edge, anon!" style={{marginTop: '44px'}} />
-        <div className={styles.buttons} style={{marginTop: '44px'}}>
-          <Link
-            className="button button--secondary button--lg"
-            to="https://github.com/newfound8ion/newcoin-evm">
-            Newcoin EVM GitHub Repo
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import { useRouteData } from '@docusaurus/useRouteData';
+import Link from '@docusaurus/Link';
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { items } = useRouteData();
+  const blogItems = items || [];
+
   return (
     <Layout
-      title={`Welcome to ${siteConfig.title}`}
-      description="Welcome to the cutting edge, anon!">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-        <img 
-          src="/img/hypernetwork.png" 
-          alt="Newcoin Hypernetwork" 
-          style={{ 
-            display: 'block', 
-            marginLeft: 'auto', 
-            marginRight: 'auto', 
-            marginTop: '0', 
-          }} 
-        />
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/getting-started/overview">
-            Get Started With Newcoin
-          </Link>
+      title="Blog Home"
+      description="List of blog posts and their content">
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ flex: '0 0 25%', borderRight: '1px solid #ddd', padding: '20px' }}>
+          <ul>
+            {blogItems.map((item, index) => (
+              <li key={index}>
+                <Link to={item.permalink}>{item.title}</Link>
+              </li>
+            ))}
+          </ul>
         </div>
-      </main>
+        <div style={{ flex: '1', padding: '20px' }}>
+          {blogItems.map((item, index) => (
+            <div key={index} id={item.permalink}>
+              <h2>{item.title}</h2>
+              <div dangerouslySetInnerHTML={{ __html: item.content }} />
+            </div>
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 }
